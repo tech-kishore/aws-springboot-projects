cb09d507a6b06bc28cda79906d2dd8e7b77e093b
Dev Profile <kishore.rajkumar@hotmail.com>
2023-07-30 13:13:28 +0530

    backend api project created

---
+++ 1.scalable-web-application-on-aws-elastic-beanstalk/ecomm-api-webapp/src/main/java/com/kishore/ecommapiwebapp/EcommApiWebappApplication.java
@@ -0,0 +1,13 @@
+package com.kishore.ecommapiwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommApiWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommApiWebappApplication.class, args);
+	}
+
+}
